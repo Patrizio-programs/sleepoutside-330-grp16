658dc1b7bcf156ebf2e26933b9557067d32eb399
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-11-16 15:28:16 -0400

    GGs

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,9 @@
-// /src/js/product.js
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { getParam, setLocalStorage } from "./utils.mjs";
 
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
