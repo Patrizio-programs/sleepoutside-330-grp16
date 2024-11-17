79a55abe51584bddca367c51737b18e4fc0fc53a
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2024-11-16 22:08:04 -0400

    Update ProductDetails.mjs

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 function productDetailsTemplate(product) {
   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
@@ -36,8 +36,11 @@ export default class ProductDetails {
       .addEventListener("click", this.addToCart.bind(this));
   }
   addToCart() {
-    setLocalStorage("so-cart", this.product);
-  }
+    let cart = getLocalStorage("so-cart") || [];
+    cart = Array.isArray(cart) ? cart : [];
+    cart.push(this.product);
+    setLocalStorage("so-cart", cart);
+}
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
     element.insertAdjacentHTML(
